f3da76c6e5012afd9ed3d06cb478ff11e6ae6146
Josh <nylaj@protonmail.com>
2023-09-17 18:18:46 +0100

    Add filmflix link, too.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -14,14 +14,23 @@ github.addEventListener("click", (e) => {
     window.location.href = "https://github.com/nylaj";
 })
 
+//housePrices link
 const housePrices = document.getElementById("housePrices");
 housePrices.addEventListener("click", (e) => {
     console.log(e);
     window.location.href = "https://github.com/groupgiteight/Predicting-House-Prices";
 })
 
+//PHP link
 const php = document.getElementById("PHP");
 php.addEventListener("click", (e) => {
     console.log(e)
     window.location.href = "https://github.com/nylaj/gamedatabase";
+})
+
+//filmflix link
+const pysql = document.getElementbyId("PyQL");
+pysql.addEventListener("click", (e) => {
+	console.log(e)
+	window.location.href = "https://github.com/nylaj/filmflixdb";
 })
